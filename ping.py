9365b67d9894daa69f2838143a236ca4f5ce8e84
Dipanshu0919 <108180019+Dipanshu0919@users.noreply.github.com>
2025-12-05 21:03:16 +0530

    Add sqlitecloud function for ping management
    Implement sqlitecloud function to manage ping records.

---
+++ ping.py
@@ -0,0 +1,28 @@
+import os, requests, json, sqlitecloud, datetime
+
+allsecrets = os.environ.get("ALL_SECRETS")
+allsecrets = json.loads(allsecrets)
+
+def sqlitecloud():
+  try:
+    keys = allsecrets.get("SQLITECLOUD", "")
+    if keys == "":
+      print("No key found")
+      return
+      
+    key_list = list(keys)
+    for key in key_list:
+      db = sqlitecloud.connect(key)
+      db.row_factory = sqlitecloud.Row
+      c = db.cursor()
+      create = "CREATE TABLE IF NOT EXISTS PINGER(LAST_PING TEXT)"
+      c.execute(create)
+      fetch = c.execute("SELECT * FROM PINGER").fetchone()
+      if fetch:
+        c.execute("UPDATE PINGER SET LAST_PING=(?)", (datetime.datetime.now(), ))
+        print(f"PING FROM {key} DONE.")
+      else:
+        c.execute("INSERT INTO PINGER(LAST_PING) VALUES=(?)", (datetime.datetime.now(), ))
+  except Exception as e:
+    print(e)
+    
